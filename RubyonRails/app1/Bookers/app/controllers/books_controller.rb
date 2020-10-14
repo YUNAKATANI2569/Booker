28777057c5e2923f8c253fd6ec617c4b26f8596e
EC2 Default User <ec2-user@ip-172-31-0-113.ec2.internal>
2020-10-14 13:19:30 +0000

    [Finish] 3-8

---
+++ RubyonRails/app1/Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  protect_from_forgery :except => [:destroy]
+  def top
+
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+     @book =Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+     if @book.save
+     redirect_to book_path(@book), notice: "Book was successfully created."
+     else
+
+        @books = Book.all
+      render action: :index#書き込み失敗
+
+     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render action: :edit#書き込み失敗
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
+  end
+
+
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
